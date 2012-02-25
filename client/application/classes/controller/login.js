e9429ca13fe61911f2fcb6f511a1d06ec9ca738a
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-02-25 15:46:57 +0200

    Remove debugging code from Login controller, #nfc.

--- client/application/classes/controller/login.js
+++ client/application/classes/controller/login.js
@@ -1,8 +1,5 @@
 define('Controller/Login', function(Controller, I18n) {
   return Controller.extend({
-    initialize: function() {
-      console.log(I18n.format('DEBUG: Controller/Login initialized.'));
-    },
     index: define(function(View_Login_Index) {
       new View_Login_Index().render({
         message: 'Hello World'
