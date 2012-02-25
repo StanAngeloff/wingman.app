20f6e67ca09e4d4bedbb409f9acd170e48ebb67e
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-02-25 15:45:04 +0200

    Use Python script to traverse and include system and application files.
    Instead of including all scripts, styles and templates by hand in the HTML
    file, use Python to perform the directory scanning. This will then
    automatically pull in any newly created files without the need to modify
    the wrapper file (index.html).

---
+++ client/system/bootstrap.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import fnmatch, os
+
+# Determine document root where system/ and application/ directories live.
+project_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Start by building a list of all available modules incl. the default 'application'.
+available_modules = []
+system_path = os.path.join(project_path, 'system', 'modules')
+for dirname in os.listdir(system_path):
+  if os.path.isdir(os.path.join(system_path, dirname)):
+    available_modules.append(os.path.join(system_path, dirname))
+available_modules.append(os.path.join(project_path, 'application'))
+
+# Scan each module in order.
+for module_path in available_modules:
+  # Define the order of files to be included in.
+  for dirname, pattern in [
+      ('bundles', '*.css'),
+      ('bundles', '*.js'),
+      ('functions.js', None),
+      ('classes', '*.js'),
+      ('templates', '*.hbs'),
+      ('bootstrap.js', None)
+    ]:
+    try_path = os.path.join(module_path, dirname)
+    if not os.path.exists(try_path):
+      continue
+    files = []
+    # If just a file, append to the list.
+    if os.path.isfile(try_path):
+      files.append((try_path, None))
+    # If a directory, scan for files matching pattern recursively.
+    else:
+      for top, directories, filename in os.walk(try_path):
+        for filename in fnmatch.filter(filename, pattern):
+          file_abspath = os.path.join(top, filename)
+          files.append((file_abspath, os.path.relpath(file_abspath, try_path)))
+      files.sort()
+    # Print all files to STDOUT.
+    for file_abspath, file_relpath in files:
+      if file_abspath.endswith('.css'):
+        print "<link rel=stylesheet href=%s>" % (os.path.relpath(file_abspath, project_path))
+      elif file_abspath.endswith('.js'):
+        print "<script src=%s></script>" % (os.path.relpath(file_abspath, project_path))
+      elif file_abspath.endswith('.hbs'):
+        print "<script id=__template-%s type=text/x-handlebars-template>%s</script>" % (os.path.splitext(file_relpath.replace(os.sep, '-'))[0], open(file_abspath).read())
