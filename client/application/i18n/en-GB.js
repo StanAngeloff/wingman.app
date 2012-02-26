682b273dff620ab3d13864978993224fdf314f6e
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-02-26 14:16:25 +0200

    Translate input boxes labels on Log in page, #nfc.

--- client/application/i18n/en-GB.js
+++ client/application/i18n/en-GB.js
@@ -2,6 +2,8 @@ define('I18n/en-GB', function() {
   return {
     'login.header': 'Welcome to Wingman.app',
     'login.instructions': 'Please log in to your account before you could continue.',
+    'login.box.email': 'E-mail',
+    'login.box.password': 'Password',
     'login.signup': 'Sign Up',
     'login.continue': 'Continue'
   };
