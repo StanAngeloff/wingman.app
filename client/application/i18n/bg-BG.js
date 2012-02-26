17a01429a39247d7537224130bf1154a4b16a6a4
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-02-26 14:30:49 +0200

    Add a reference translation to Bulgaria incl. system messages.

---
+++ client/application/i18n/bg-BG.js
@@ -0,0 +1,19 @@
+define('I18n/bg-BG', function() {
+  return {
+    'login.header': 'Добре дошли в Wingman.app',
+    'login.instructions': 'Моля идентифицирайте се по-долу преди да продължите.',
+    'login.box.email': 'И-мейл Адрес',
+    'login.box.password': 'Парола',
+    'login.signup': 'Нов Потребител',
+    'login.continue': 'Вход',
+
+    "':method' could not find an element for template ':template', DOM query was ':query'.": "':method' не може да открие елемент за темплейт ':template', DOM заявката беше ':query'.",
+    "':method' could not find element to update, DOM query was ':query'.": "':method' не можа да открие елемент за промяна, DOM заявката беше ':query'.",
+    "':callee' expects ':name' to be of type ':type'.": "':callee' очаква ':name' да бъде от тип ':type'.",
+    "':callee' expects ':name' to have a value for ':key'.": "':callee' очаква ':name' да съдържа стойност за ':key'.",
+    "Method ':method' in class ':class' is marked as abstract and must be implemented in derived classes.": "Метод ':method' в клас ':class' е маркиран като абстрактен и трябва да бъде имплементиран в нови класове.",
+    "Route ':route' (:name) failed as the requested controller does not have a method for action ':action' defined.": "Път ':route' (:name) не беше успешен понеже избрания контролер не дефинира метод за действие ':action'.",
+    "GET ':key' failed as there is no controller with that action registered.": "GET ':key' не беше успешен понеже няма регистриран контролер с това действие.",
+    "':method' called without any routes set up. See ':relative'.": "':method' беше извикан преди да бъдат дефинирани пътища. Виж ':relative'."
+  };
+});
