2166d759569f018e5cab224960a95b8f1ad0418e
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-03-10 18:18:42 +0200

    Add Crypto class which exports the 'sjcl' object (see previous commits).

---
+++ client/system/modules/crypto/classes/crypto.js
@@ -0,0 +1,10 @@
+define('Crypto',
+/**
+ * The Crypto module is a shim on top of Stanford Javascript Crypto Library.
+ *
+ * @exports Crypto
+ * @see http://crypto.stanford.edu/sjcl/
+ */
+function() {
+  return sjcl;
+});
