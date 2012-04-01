15010be9b6cb2f0544bb4129c28ebbf42002d147
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-04-01 18:39:01 +0300

    Don't wrap translations in a function, define as object instead, #nfc.

--- client/application/i18n/en-GB.js
+++ client/application/i18n/en-GB.js
@@ -1,21 +1,19 @@
-define('i18n/en-GB', function() {
-  return {
-    'login.header': 'Welcome to Wingman.app',
-    'login.instructions': 'Please log in to your account before you could continue.',
-    'form.login[email]': 'E-mail',
-    'form.login[password]': 'Password',
-    'login.signup': 'Sign Up',
-    'login.continue': 'Continue',
+define('i18n/en-GB', {
+  'login.header': 'Welcome to Wingman.app',
+  'login.instructions': 'Please log in to your account before you could continue.',
+  'form.login[email]': 'E-mail',
+  'form.login[password]': 'Password',
+  'login.signup': 'Sign Up',
+  'login.continue': 'Continue',
 
-    'signup.header': 'Sign Up to Wingman.app',
-    'signup.instructions': 'Please fill in the details below and select ‘Sign Up’ when you are ready to complete your account.',
-    'form.signup[email]': 'E-mail',
-    'form.signup[password]': 'Password',
-    'form.signup[password2]': 'Password (verify)',
-    'signup.cancel': 'Cancel',
-    'signup.continue': 'Sign Up',
+  'signup.header': 'Sign Up to Wingman.app',
+  'signup.instructions': 'Please fill in the details below and select ‘Sign Up’ when you are ready to complete your account.',
+  'form.signup[email]': 'E-mail',
+  'form.signup[password]': 'Password',
+  'form.signup[password2]': 'Password (verify)',
+  'signup.cancel': 'Cancel',
+  'signup.continue': 'Sign Up',
 
-    'validity.minlength': 'Please enter at least :length characters.',
-    'validity.password.match': 'Please re-type the password as it does not match.'
-  };
+  'validity.minlength': 'Please enter at least :length characters.',
+  'validity.password.match': 'Please re-type the password as it does not match.'
 });
