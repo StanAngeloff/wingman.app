9da7cc284c708a2515df02c82a2f78065056b5eb
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-04-07 21:06:41 +0300

    Use Unicode strings, #nfc.

--- server/wingman/blueprints/user.py
+++ server/wingman/blueprints/user.py
@@ -6,8 +6,8 @@ user = Blueprint('user', __name__, url_prefix='/user')
 
 @create(user, '')
 def create():
-    return jsonify(message='user.create()')
+    return jsonify(message=u'user.create()')
 
 @retrieve(user, '/<int:id>')
 def get(id):
-    return jsonify(message='user.get(%s)' % id)
+    return jsonify(message=u'user.get(%s)' % id)
