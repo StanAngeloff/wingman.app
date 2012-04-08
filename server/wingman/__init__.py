1d6715f73b08944dca6e3cc912ed2ea371b2ecf8
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-04-08 09:10:01 +0300

    Add error handlers which return JSON to the client (instead of HTML).

--- server/wingman/__init__.py
+++ server/wingman/__init__.py
@@ -1,6 +1,6 @@
 from .blueprints.user import user
 from .storage import create_storage
-from flask import Flask, abort, request
+from flask import Flask, abort, jsonify, request
 
 
 def create_app(config_filepath=None, import_name=None):
@@ -9,6 +9,7 @@ def create_app(config_filepath=None, import_name=None):
     app = Flask(import_name, instance_relative_config=True)
     _configure_app(app, config_filepath, import_name)
     _jsonify_app(app)
+    _install_error_handlers(app)
     _register_blueprints(app)
     create_storage(app)
     return app
@@ -30,6 +31,22 @@ def _jsonify_app(app):
     def after_request(response):
         response.headers['Content-Type'] = app.config['DEFAULT_MIME_TYPE']
         return response
+
+def _install_error_handlers(app):
+    known_errors = [
+        (400, u'The request could not be understood by the server due to malformed syntax.'),
+        (404, u'The server has not found anything matching the Request-URI.'),
+        (500, u'The server encountered an unexpected condition which prevented it from fulfilling the request. Please try again and if the issue persists, contact the administrator for assistance.'),
+    ]
+    def make_error_handler(code, message):
+        @app.errorhandler(code)
+        def error_handler(error):
+            response = jsonify({ 'code': code, 'message': message })
+            response.status_code = code
+            return response
+        return error_handler
+    for code, message in known_errors:
+        make_error_handler(code, message)
 
 def _register_blueprints(app):
     app.register_blueprint(user)
