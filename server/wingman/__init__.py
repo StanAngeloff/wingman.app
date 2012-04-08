ccfd46f8254f94aa0ed469133b5cf97baccf9715
Stan Angeloff <stanimir@psp-webtech.co.uk>
2012-04-08 08:55:36 +0300

    Return a 400 Bad Request is no JSON was submitted.

--- server/wingman/__init__.py
+++ server/wingman/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort, request
 
 
 def create_app(config_filepath=None, import_name=None):
@@ -9,6 +9,11 @@ def create_app(config_filepath=None, import_name=None):
     if config_filepath is not None:
         app.config.from_pyfile(config_filepath)
     app.config.from_envvar(('%s_SETTINGS' % import_name.upper()), silent=True)
+
+    @app.before_request
+    def before_request():
+        if request.method != 'OPTIONS' and request.json is None:
+            abort(400)
 
     @app.after_request
     def after_request(response):
